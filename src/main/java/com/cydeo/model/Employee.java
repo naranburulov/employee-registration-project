78664c3b1b48494cd874111ed9d6d4d417a86211
naranburulov <naranburulov@gmail.com>
2022-09-27 15:50:04 -0400

    validations added

--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -3,8 +3,11 @@ package com.cydeo.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
@@ -12,7 +15,19 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class Employee {
 
+//    @NotNull              --->    field shouldn't be null
+//    @NotEmpty             --->    field shouldn't be ""
+//    @NotBlank             --->    field shouldn't be "    "
+
+//    @NotNull              --->    @NotNull
+//    @NotEmpty             --->    @NotNull+@NotEmpty
+//    @NotBlank             --->    @NotNull+@NotEmpty+@NotBlank
+
+    @NotBlank
+    @Size(max=12, min=2)
     private String firstName;
+    @NotBlank
+    @Size(max=12, min=2)
     private String lastName;
 
     //Thymeleaf accepts yyyy-MM-dd, but LocalDate accepts differently!
