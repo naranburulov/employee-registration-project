f9a7133b5d2f9a094c1d484b17b2d632e47b7bb2
naranburulov <naranburulov@gmail.com>
2022-09-27 11:16:50 -0400

    Employee Class created with all the fields for the web-form

--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -1,2 +1,27 @@
-package com.cydeo.model;public class Employee {
+package com.cydeo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDay;
+    private String email;
+    private String password;
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
+
+
+
 }
